cc838488c4d26e7c6046fba8831b6408575c0dda
helywin <jiang770882022@hotmail.com>
2020-07-12 15:57:24 +0800

    finish template deducing type

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Effective Modern C++笔记
 
+1.模板型别推导 (代码)[code/1-template_deducing_type]
---
+++ code/1-template_deducing_type/template.cpp
@@ -0,0 +1,131 @@
+//
+// Created by helywin on 2020/7/12.
+//
+
+#include "template.hpp"
+#include <iostream>
+#include <boost/type_index.hpp>
+#include <iomanip>
+
+using namespace std;
+using boost::typeindex::type_id_with_cvr;
+
+#define FUNC_BODY \
+cout.setf(ios::left); \
+cout.fill(' '); \
+cout << setw(25) << "T = " + type_id_with_cvr<T>().pretty_name(); \
+cout << setw(25) << "param = " + type_id_with_cvr<decltype(param)>().pretty_name() << endl;
+
+#define IN_TYPE(type) \
+cout.setf(ios::left); \
+cout.fill(' '); \
+cout << setw(25) << "IN = " + type_id_with_cvr<decltype(type)>().pretty_name();
+
+template<typename T>
+void func(T param)
+{
+    FUNC_BODY
+}
+
+template<typename T>
+void func_ref(T &param)
+{
+    FUNC_BODY
+}
+
+template<typename T>
+void func_const_ref(const T &param)
+{
+    FUNC_BODY
+}
+
+template<typename T>
+void func_pointer(T *param)
+{
+    FUNC_BODY
+}
+
+template<typename T>
+void func_universal_ref(T &&param)
+{
+    FUNC_BODY
+}
+
+template<typename T, std::size_t N>
+constexpr std::size_t arraySize(T (&)[N]) noexcept
+{
+    return N;
+}
+
+void deducing_template()
+{
+    int x = 0;
+    const int c_x = x;
+    const int &cr_x = x;
+    const int *cp_x = &x;
+    const int *const cpc_x = &x;
+    const char *const cpc_s = "123456";
+    const char cp_s[] = "123456";
+    cout << "----ref----" << endl;
+    IN_TYPE(x)
+    func_ref(x);
+    IN_TYPE(c_x)
+    func_ref(c_x);
+    IN_TYPE(cr_x)
+    func_ref(cr_x);
+
+    cout << "----const_ref----" << endl;
+    IN_TYPE(x)
+    func_const_ref(x);
+    IN_TYPE(c_x)
+    func_const_ref(c_x);
+    IN_TYPE(cr_x)
+    func_const_ref(cr_x);
+
+    cout << "----pointer----" << endl;
+    IN_TYPE(x)
+    func_pointer(&x);
+    IN_TYPE(cp_x)
+    func_pointer(cp_x);
+    IN_TYPE(cpc_x)
+    func_pointer(cpc_x);
+    IN_TYPE(cpc_s)
+    func_pointer(cpc_s);
+    IN_TYPE(cp_s)
+    func_pointer(cp_s);
+
+    cout << "----universal_ref----" << endl;
+    IN_TYPE(x)
+    func_universal_ref(x);
+    IN_TYPE(c_x)
+    func_universal_ref(c_x);
+    IN_TYPE(cr_x)
+    func_universal_ref(cr_x);
+    IN_TYPE(0)
+    func_universal_ref(0);
+
+    cout << "----copy----" << endl;
+    IN_TYPE(x)
+    func(x);
+    IN_TYPE(c_x)
+    func(c_x);
+    IN_TYPE(cr_x)
+    func(cr_x);
+    IN_TYPE(0)
+    func(0);
+    IN_TYPE(cp_x)
+    func(cp_x);
+    IN_TYPE(cpc_x)
+    func(cpc_x);
+    IN_TYPE(cpc_s)
+    func(cpc_s);
+    IN_TYPE(cp_s)
+    func(cp_s);
+
+    cout << "array size: " << arraySize(cp_s) << endl;
+}
+
+int main()
+{
+    deducing_template();
+}
